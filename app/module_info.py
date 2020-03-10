3c973c27c9de9712be16fbdae7c3cb93b4a29bd5
yeeh <yeeh1616@gmail.com>
2020-03-10 01:14:30 +0100

    Merge remote-tracking branch 'github/YeYeYe' into YeYeYe
    # Conflicts:
    #	project_database

--- app/module_info.py
+++ app/module_info.py
@@ -1,8 +1,10 @@
 from flask import (
     Blueprint, flash, redirect, render_template, url_for,
     session, request)
+from flask_login import login_required, current_user
 
 from app import db
+from app.decorators import check_confirmed
 from app.forms import ModuleInfoForm, CommentForm
 from app.models import Module, User, Comment, get_avg_stars, add_comment_by_entity
 
@@ -26,35 +28,23 @@ def test3():
     return render_template('modules_info.html', module=None)
 
 
-# @bp.route('/info', methods=['GET', 'POST'])
-# # @login_required
-# def info(id):
-#     '''
-#     1.根据 id 查找 module 对象
-#     2.返回module页面
-#     '''
-#     module = Module.query.filter_by(id=id).first()
-#     return render_template('module_info.html', module=module)
-
-
 @bp.route('/info', methods=['GET', 'POST'])
-# @login_required
+@login_required
+@check_confirmed
 def info():
-    tempUserId = '1'
-    moduleId = request.args.get("id")
-    # 存module id 到 session
-    session['moduleId'] = 'moduleId'
-    userId = tempUserId
-    module = Module.query.filter_by(id=moduleId).first()
-    user = User.query.filter_by(id=userId).first()
-    # commentList = Comment.query.filter_by(module_id=moduleId).all()
-    commentList = db.session.query(User.id, User.uname, User.img, Comment.content, Comment.module_id).filter(Comment.owner_id == User.id).filter(Comment.module_id==moduleId).all()
-    avg_star = get_avg_stars(moduleId)
-    return render_template('module_info.html', module=module, user=user, commentList=commentList, totalComments=len(commentList), avgStar=avg_star)
+    module_id = request.args.get("id")
+    session['moduleId'] = module_id
+    user_id = current_user.id
+    module = Module.query.filter_by(id=module_id).first()
+    user = User.query.filter_by(id=user_id).first()
+    comment_list = db.session.query(User.id, User.uname, User.img, Comment.content, Comment.module_id).filter(Comment.owner_id == User.id).filter(Comment.module_id==module_id).all()
+    avg_star = get_avg_stars(module_id)
+    return render_template('module_info.html', module=module, user=user, commentList=comment_list, totalComments=len(comment_list), avgStar=avg_star)
 
 
 @bp.route('/edit', methods=['GET', 'POST'])
-# @login_required
+@login_required
+@check_confirmed
 def edit():
     form = ModuleInfoForm()
     module = Module.query.filter_by(id=form.id.data).first()
@@ -62,27 +52,28 @@ def edit():
 
 
 @bp.route('/save', methods=['GET', 'POST'])
-# @login_required
+@login_required
+@check_confirmed
 def save():
     moduleInfoForm = ModuleInfoForm()
-    module = Module(moduleInfoForm.id.data, moduleInfoForm.name.data, moduleInfoForm.description.data)
+    module_id = moduleInfoForm.id.data
+    module_name = moduleInfoForm.name.data
+    module_desc = moduleInfoForm.description.data
+    module = Module(module_id, module_name, module_desc, None)
     # if moduleInfoForm.validate_on_submit():
     if Module.update_a_module_by_enity(module):
-        return render_template('module_info.html', module=module)
+        return redirect(url_for('module_info.info', id=module_id))
     else:
-        return 'Dam..........'
+        return 'save module info error ..........'
 
 
 @bp.route('/comment', methods=['GET', 'POST'])
+@login_required
+@check_confirmed
 def comment():
-    form = CommentForm()
-    comment = Comment(0, '3', form.comment.data, 0, 0)
-    # comment.stars = form.stars
-    # comment.owner_id = session.get('uid')
-    # comment.module_id = session.get('module_id')
-    # comment.owner_id = '1'
-    # comment.module_id = '3'
-    # comment.stars = 0
-    # comment.content = form.comment
+    owner_id = current_user.id
+    module_id = session.get('moduleId')
+    content = CommentForm().comment.data
+    comment = Comment(owner_id, module_id, content, 0, 0)
     add_comment_by_entity(comment)
-    return 'True'
+    return redirect(url_for('module_info.info', id=module_id))
