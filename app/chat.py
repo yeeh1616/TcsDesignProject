b0247a085005c0cca51046b39640ae5b0884d2cc
yeeh <yeeh1616@gmail.com>
2020-04-04 02:54:13 +0200

    Merge remote-tracking branch 'github/YeYeYe' into YeYeYe
    # Conflicts:
    #	project_database

--- app/chat.py
+++ app/chat.py
@@ -1,10 +1,10 @@
 from flask import Flask, Blueprint, render_template, request
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask_socketio import SocketIO, send
 
 from app import app
 from app.decorators import check_confirmed
-from app.models import Config
+from app.models import Config, Student
 
 bp = Blueprint('chat', __name__, template_folder='templates/chat')
 app.config['SECRET_KEY'] = 'mysecret'
@@ -13,7 +13,7 @@ socketio = SocketIO(app)
 
 @socketio.on('message')
 def handle_message(msg):
-    print('Message: ' + msg)
+    # print('Message: ' + msg)
     send(msg, broadcast=True)
 
 
@@ -23,4 +23,5 @@ def handle_message(msg):
 def chat_page():
     config = Config.get_config_by_key('server')
     config.ip = request.remote_addr
-    return render_template('chat/chat.html', config=config)
+    user = Student.get_full_info_by_id(current_user.id)
+    return render_template('chat/chat.html', config=config, user=user)
