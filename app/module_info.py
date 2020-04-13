ad498370c126bbb76bd35f9101ae60a996df235c
yeeh1616 <yeeh1616@gmail.com>
2020-04-13 19:09:19 +0200

    Merge remote-tracking branch 'github/LichenXia2' into Yeeh03
    # Conflicts:
    #	app/models.py

--- app/module_info.py
+++ app/module_info.py
@@ -80,7 +80,6 @@ def info():
                                avgStar=avg_star,
                                notification_num=notification_num,
                                title=title,
-                               is_tourist=is_tourist,
                                questionnaire=questionnaire,
                                star_dict=json.dumps(star_dict))
 
